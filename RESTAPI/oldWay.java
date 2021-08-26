d48ed2ec14ea8a58af096a2fbf9c315f53e8dda8
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-08-26 04:26:19 +0200

    Create oldWay.java

---
+++ RESTAPI/oldWay.java
@@ -0,0 +1,57 @@
+package com.example;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+	
+	private static HttpURLConnection connection;
+	
+	
+	public static void main(String[] args) {
+		//Method 1: java.net.HttpURLConnection
+		
+		BufferedReader reader;
+		String line;
+		StringBuffer responseContent = new StringBuffer();
+		
+		try {
+			URL url = new URL("https://api.chucknorris.io/jokes/random");
+			
+			connection = (HttpURLConnection) url.openConnection();
+			
+			//Request setup
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);
+			connection.setReadTimeout(5000);
+			
+			int status = connection.getResponseCode();
+			
+			if (status > 299) {
+				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			} else {
+				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			}
+			System.out.println(responseContent.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			connection.disconnect();
+		}
+	}
+}
