a6e9e33c30a8693d3d8c0d2ee99c7baf141f56a6
kristianseng <62358534+kristianseng@users.noreply.github.com>
2021-08-27 03:40:35 +0200

    Create stackTraceCheck.java

---
+++ Threads&StackTrace/stackTraceCheck.java
@@ -0,0 +1,37 @@
+public class ExceptionExample
+{
+  public static void main(String[] args)
+  {
+    method1();
+  }
+
+  public static void method1()
+  {
+    method2();
+  }
+
+  public static void method2()
+  {
+    method3();
+  }
+
+  public static void method3()
+  {
+     StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+    for (StackTraceElement element : stackTraceElements)
+    {
+       System.out.println(element.getMethodName());
+    }
+  }
+}
+
+
+/*
+
+getStackTrace
+method3
+method2
+method1
+main
+
+*/
